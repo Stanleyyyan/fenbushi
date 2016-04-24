a6475674486b7fb74766a1c8e60be25188875587
maounan <dennis_mon@163.com>
2016-04-23 21:29:24 -0500

    DoPingV2

--- src/libkademlia/routing_table.go
+++ src/libkademlia/routing_table.go
@@ -4,100 +4,96 @@ package libkademlia
 // as a receiver for the RPC methods, which is required by that package.
 
 import (
-	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"net/rpc"
-	"strconv"
-	"container/list"
-	"math"
+	// "fmt"
+	// "log"
+	// "net"
+	// "net/http"
+	// "net/rpc"
+	// "strconv"
+	// "container/list"
+	// "math"
 )
 
-const (
-	alpha = 3
-	b     = 8 * IDBytes
-	k     = 20
-)
 
 // Kademlia type. You can put whatever state you need in this.
 type RoutingTable struct {
 	
-	var buckets	[][]Contact
+	buckets	[][]Contact
 	//Self_contact	Contact
 	addChan 	chan Contact
 	delChan 	chan Contact
 
 }
 
-func NewRoutingTable(node Contact) *Routing_Table {
-	tb := new(Routing_Table)
+func NewRoutingTable(node Contact) *RoutingTable {
+	tb := new(RoutingTable)
 	tb.buckets = make([][]Contact, 160)
-	tb.addChan = make(chan *Contact)
-	tb.delChan = make(chan *Contact)
+	// tb.addChan = make(chan *Contact)
+	// tb.delChan = make(chan *Contact)
+	return tb
 }
 
-func (tb *Routing_Table) HandleChange() {
-	for{
-		select {
-			//Add Contact
-		case c := <-tb.addChan:
-			tb.AddContact(c)
-		case c := <-tb.delChan:
-			tb.DelContact(c)
-		}
-	}
-}
+// func (tb *Routing_Table) HandleChange() {
+// 	for{
+// 		select {
+// 			//Add Contact
+// 		case c := <-tb.addChan:
+// 			tb.AddContact(c)
+// 		case c := <-tb.delChan:
+// 			tb.DelContact(c)
+// 		}
+// 	}
+// }
 
 
-func (tb *Routing_Table) AddContact(c Contact) {
-	dis := tb.NodeID.Xor(c.NodeID)
-	numOfBucket := 159 - dis.PrefixLen()
-	tb.buckets[numberOfBucket]
-	addHelper(numberOfBucket, tb, c)
-}
+// func (tb *Routing_Table) AddContact(c Contact) {
+// 	dis := tb.NodeID.Xor(c.NodeID)
+// 	numOfBucket := 159 - dis.PrefixLen()
+// 	tb.buckets[numberOfBucket]
+// 	addHelper(numberOfBucket, tb, c)
+// }
 
 
-func addHelper(numOfBucket int, tb RoutingTable, c Contact) {
-	containsC := false
-	idx := 0
-	for index, c1 := range tb.buckets[numOfBucket] {
-        if c1.NodeID == c.NodeID {
-            containsC = true
-						idx = index
-        }
-  }
-	if containsC {
-		tb.buckets[numOfBucket] = tb.buckets[numOfBucket][:,idx - 1] +
-									tb.buckets[numOfBucket][idx + 1,:] + tb.buckets[numOfBucket][idx]
-	} else {
-		if len(tb.buckets[numOfBucket]) < 20 {
-			tb.buckets[numOfBucket].append(c)
-		} else {
-			// TODO:ping
+// func addHelper(numOfBucket int, tb RoutingTable, c Contact) {
+// 	containsC := false
+// 	idx := 0
+// 	for index, c1 := range tb.buckets[numOfBucket] {
+//         if c1.NodeID == c.NodeID {
+//             containsC = true
+// 						idx = index
+//         }
+//   }
+// 	if containsC {
+// 		tb.buckets[numOfBucket] = tb.buckets[numOfBucket][:idx - 1] +
+// 									tb.buckets[numOfBucket][idx + 1:] + tb.buckets[numOfBucket][idx]
+// 	} else {
+// 		if len(tb.buckets[numOfBucket]) < 20 {
+// 			tb.buckets[numOfBucket].append(c)
+// 		} else {
+// 			// TODO:ping
 
-		}
-	}
-}
+// 		}
+// 	}
+// }
 
-func (tb Routing_Table)DelContact(c *Contact) {
-	dis := tb.NodeID.Xor(c.NodeID)
-	numOfBucket := 159 - dis.PrefixLen()
-	tb.buckets[numberOfBucket]
-	delHelper(numberOfBucket, tb, c)
-}
+// func (tb Routing_Table)DelContact(c *Contact) {
+// 	dis := tb.NodeID.Xor(c.NodeID)
+// 	numOfBucket := 159 - dis.PrefixLen()
+// 	tb.buckets[numberOfBucket]
+// 	delHelper(numberOfBucket, tb, c)
+// }
 
 
-func delHelper(numOfBucket int, tb RoutingTable, c Contact) {
-	containsC := false
-	idx := 0
-	for index, c1 := range tb.buckets[numOfBucket] {
-        if c1.NodeID == c.NodeID {
-            containsC = true
-			idx = index
-        }
-    }
-	if containsC {
-		tb.buckets[numOfBucket] = tb.buckets[numOfBucket][:,idx - 1] + tb.buckets[numOfBucket][idx + 1,:]
-	}
-}
+// func delHelper(numOfBucket int, tb RoutingTable, c Contact) {
+// 	containsC := false
+// 	idx := 0
+// 	for index, c1 := range tb.buckets[numOfBucket] {
+//         if c1.NodeID == c.NodeID {
+//             containsC = true
+// 			idx = index
+//         }
+//     }
+// 	if containsC {
+// 		tb.buckets[numOfBucket] = tb.buckets[numOfBucket][:,idx - 1] + tb.buckets[numOfBucket][idx + 1,:]
+// 	}
+// }
